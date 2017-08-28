1ee02d4a7de9cee3ed784a0d5602cc1c73b728d8
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-08-28 15:57:33 +0600

    Splash Activity added

--- app/src/main/java/com/educareapps/quiz/activities/MainActivity.java
+++ app/src/main/java/com/educareapps/quiz/activities/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.educareapps.quiz.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
