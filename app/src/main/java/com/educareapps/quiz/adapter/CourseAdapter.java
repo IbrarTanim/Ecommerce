ea4e6daec6415890276cc7b6d1436173345ba1cd
tajmulalam <taj.cse37@gmail.com>
2017-09-10 17:10:45 +0600

    course show done

---
+++ app/src/main/java/com/educareapps/quiz/adapter/CourseAdapter.java
@@ -0,0 +1,77 @@
+package com.educareapps.quiz.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.educareapps.quiz.R;
+import com.educareapps.quiz.dao.QuestionSetTable;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rakib on 9/10/2017.
+ */
+
+public class CourseAdapter extends BaseAdapter {
+    ArrayList<QuestionSetTable> questionSetTableArrayList;
+    Context context;
+    private static LayoutInflater inflater = null;
+
+    public CourseAdapter(ArrayList<QuestionSetTable> questionSetTableArrayList, Context context) {
+        this.questionSetTableArrayList = questionSetTableArrayList;
+        this.context = context;
+        inflater = (LayoutInflater) context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @Override
+    public int getCount() {
+        return questionSetTableArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return questionSetTableArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return questionSetTableArrayList.get(position).getQuestion_set_id();
+    }
+
+    public class Holder {
+        TextView tvCourseTitle;
+        ImageView ivCoursePhoto;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Holder holder = new Holder();
+        View rowView;
+        rowView = inflater.inflate(R.layout.course_item, null);
+        holder.tvCourseTitle = (TextView) rowView.findViewById(R.id.tvCourseTitle);
+        holder.ivCoursePhoto = (ImageView) rowView.findViewById(R.id.ivCoursePhoto);
+        holder.tvCourseTitle.setText(questionSetTableArrayList.get(position).getTitle());
+        Picasso.with(context).load(questionSetTableArrayList.get(position).getPhoto()).placeholder(R.drawable.ic_logo).error(R.drawable.ic_logo).into(holder.ivCoursePhoto);
+        holder.ivCoursePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTestActivity();
+            }
+
+
+        });
+        return rowView;
+    }
+
+    private void goToTestActivity() {
+
+    }
+}
